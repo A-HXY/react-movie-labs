5ad5f7a28732a07a910a1a40d8262ea7a6bab814
Xinyue <858937756@qq.com>
2024-11-24 19:32:48 +0000

    Perfect Favourite and Watchlist.

--- movies/src/context/moviesContext.js
+++ movies/src/context/moviesContext.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getFavouriteMovies } from '../api/tmdb-api';
+import { getWatchlistMovies } from '../api/tmdb-api';
+import { toggleFavorite } from '../api/tmdb-api';
+import { toggleWatchlist } from '../api/tmdb-api';
 
 export const MoviesContext = React.createContext(null);
 
@@ -8,6 +12,7 @@ const MoviesContextProvider = (props) => {
   const [mustWatch, setMustWatch] = useState( [] )
 
   const addToFavorites = (movie) => {
+    const sessionId = sessionStorage.getItem("sessionId");
     let newFavorites = [];
     if (!favorites.includes(movie.id)){
       newFavorites = [...favorites, movie.id];
@@ -16,15 +21,63 @@ const MoviesContextProvider = (props) => {
       newFavorites = [...favorites];
     }
     setFavorites(newFavorites)
+    toggleFavorite(sessionId, movie.id, true)
+    .catch((error) => console.error("Error adding to favorites:", error));
   };
   
+  useEffect(() => {
+    const sessionId = sessionStorage.getItem("sessionId");
+    const fetchFavoriteMovies = async () => {
+      try {
+        const data = await getFavouriteMovies(sessionId);
+        const movieIds = data.results.map(movie => movie.id);
+        setFavorites(movieIds);
+      } catch (error) {
+        console.error("Error fetching favorite movies:", error);
+      }
+    };
+    fetchFavoriteMovies();
+  }, []);
+
   const removeFromFavorites = (movie) => {
     setFavorites( favorites.filter(
       (mId) => mId !== movie.id
     ) )
   };
 
+  const addToWatchlist = (movie) => {
+    const sessionId = sessionStorage.getItem("sessionId");
+    if (!mustWatch.includes(movie.id)) {
+      setMustWatch([...mustWatch, movie.id]);
+      toggleWatchlist(sessionId, movie.id, true)
+        .catch((error) => console.error("Error adding to watchlist:", error));
+    }
+  };
   
+
+  useEffect(() => {
+    const sessionId = sessionStorage.getItem("sessionId");
+    const fetchWatchlistMovies = async () => {
+      try {
+        const data = await getWatchlistMovies(sessionId);
+        const movieIds = data.results.map(movie => movie.id);
+        setMustWatch(movieIds);
+      } catch (error) {
+        console.error("Error fetching favorite movies:", error);
+      }
+    };
+    fetchWatchlistMovies();
+  }, []);
+
+    const removeFromWatchlist = (movie) => {
+      const sessionId = sessionStorage.getItem("sessionId");
+      setMustWatch( mustWatch.filter(
+        (mId) => mId !== movie.id
+      ) )
+      toggleWatchlist(sessionId, movie.id, false)
+        .catch((error) => console.error("Error removing from watchlist:", error))
+    };
+
   const addReview = (movie, review) => {
     setMyReviews( {...myReviews, [movie.id]: review } )
   };
@@ -49,6 +102,8 @@ const MoviesContextProvider = (props) => {
         mustWatch,
         addToFavorites,
         removeFromFavorites,
+        addToWatchlist,
+        removeFromWatchlist,
         addReview,
         addToMustWatch,
       }}
